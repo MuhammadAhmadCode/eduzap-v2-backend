bf600b92fd099b791b9255ddd530e4a12433446d
MuhammadAhmadCode <ma2565983@gmail.com>
2026-04-04 13:55:59 +0500

    add cors as middleware

--- src/app.js
+++ src/app.js
@@ -9,7 +9,11 @@ const app = express()
 
 app.use(cookieParser());
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 //auth routes
 app.use("/api/auth",authRoutes)

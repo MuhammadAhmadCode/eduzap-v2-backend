17f0fa961239a0ca59fb7799c8af20ad938632d3
MuhammadAhmadCode <ma2565983@gmail.com>
2026-04-04 13:57:30 +0500

    add auth Middleware to track logged in user

---
+++ src/Middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+function authMiddleware(req, res, next) {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ message: "Unauthorized" });
+  }
+}
+
+module.exports = authMiddleware;

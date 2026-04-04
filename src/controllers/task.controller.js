7ab0e873c87cb4f9ea0ebd0d177f356f116a0a8e
MuhammadAhmadCode <ma2565983@gmail.com>
2026-04-04 14:02:58 +0500

    prettier formation done

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,50 +1,65 @@
 const taskModel = require("../models/task.model");
 
 async function CreateTask(req, res) {
-    const { title, completed } = req.body;
+  const { title, completed } = req.body;
 
-    await taskModel.create({
-        title, completed
-    })
-    res.status(201).json({
-        message: "task Created",
-        task: title, completed
-    })
+  await taskModel.create({
+    title,
+    completed,
+    user: req.user._id,
+  });
+  res.status(201).json({
+    message: "task Created",
+    task: title,
+    completed,
+  });
 }
-
 
 async function getTasks(req, res) {
-    const tasks = await taskModel.find()
-    res.status(200).json({ message: "fetched", tasks: tasks })
+  const tasks = await taskModel.find({ user: req.user._id });
+  res.status(200).json({ message: "fetched", tasks: tasks });
 }
-
 
 async function deleteTask(req, res) {
-    try {
-        const id = req.params.id
-        const task = await taskModel.findOneAndDelete({ _id: id })
-        res.status(200).json({
-            message: "Task Deleted successfully!",
-            task: task
-        })
-    } catch (err) {
-        res.json({message:"error",err})
-    }
+  try {
+    const id = req.params.id;
+    const task = await taskModel.findOneAndDelete({
+      _id: id,
+      user: req.user._id,
+    });
+    res.status(200).json({
+      message: "Task Deleted successfully!",
+      task: task,
+    });
+  } catch (err) {
+    res.json({ message: "error", err });
+  }
 }
 
-async function updateTask(req,res){
-    const title = req.body.edit
-    const id = req.params.id
-    const task = await taskModel.findOneAndUpdate({_id:id},{title:title})
-    res.status(200).json({message:"updated successfully!",task:title})
+async function updateTask(req, res) {
+  const title = req.body.edit;
+  const id = req.params.id;
+  const task = await taskModel.findOneAndUpdate(
+    { _id: id, user: req.user._id },
+    { title: title },
+  );
+  res.status(200).json({ message: "updated successfully!", task: title });
 }
 
-
-async function handleCompleteTak(req,res){
-    const id = req.params.id
-    const completed = req.body.completed
-    await taskModel.findOneAndUpdate({_id:id},{completed:completed})
-    res.status(200).json({message:"updated successfully!"})
+async function handleCompleteTak(req, res) {
+  const id = req.params.id;
+  const completed = req.body.completed;
+  await taskModel.findOneAndUpdate(
+    { _id: id, user: req.user._id },
+    { completed: completed },
+  );
+  res.status(200).json({ message: "updated successfully!" });
 }
 
-module.exports = { CreateTask, getTasks, deleteTask,updateTask,handleCompleteTak }
+module.exports = {
+  CreateTask,
+  getTasks,
+  deleteTask,
+  updateTask,
+  handleCompleteTak,
+};
